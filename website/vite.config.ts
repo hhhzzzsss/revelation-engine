e5181e7437ec3887343af70441b53bd56562a560
hhhzzzsss <hyzhou42@gmail.com>
2026-03-28 15:34:18 -0500

    Move base in vite config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,7 +5,7 @@ import wasm from 'vite-plugin-wasm';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/revelationengine/',
+  base: '/',
   plugins: [react(), tailwindcss(), wasm()],
   worker: {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
